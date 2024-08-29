0532b0057c70072d345978611ec1a012253ff7b9
Azizbek Rahimjonov <azizbekrahimjonov571@gmail.com>
2024-08-29 17:41:50 +0500

    Update main.py
    id update

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ from geopy.distance import geodesic
 
 from append import *
 
-ADMIN_ID = 1486580350
+# ADMIN_ID = 1486580350 # Azizbek Rahimjonov
+ADMIN_ID = 456060838
 WORK_LOCATION = (41.30278475883332, 69.31477190655004)
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
