a8dd57a1751ef3b2a20237ebc47efce16f6c7c9c
azizbekrakhimjonov <azizbekrahimjonov571@gmail.com>
2024-10-14 19:09:37 +0500

    telegram id update

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import logging
 from geopy.distance import geodesic
 from append import *
 
-ADMIN_ID = 1486580350  # Azizbek Rahimjonov
-# ADMIN_ID = 456060838
+# ADMIN_ID = 1486580350  # Azizbek Rahimjonov
+ADMIN_ID = 456060838
 WORK_LOCATION = (41.30278475883332, 69.31477190655004)
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
